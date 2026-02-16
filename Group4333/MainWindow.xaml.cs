c2c8375a3031980286889c606fbe5487771ab09b
azaliya <azaliyamillion@mail.ru>
2026-02-17 01:40:27 +0300

    Добавление информации об авторе

--- Group4333/MainWindow.xaml.cs
+++ Group4333/MainWindow.xaml.cs
@@ -6,5 +6,11 @@ namespace Group4333
     {
         public MainWindow()
             => InitializeComponent();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SadrievaAzalyia4333 sadrieva = new SadrievaAzalyia4333();
+            sadrieva.Show();
+        }
     }
 }
